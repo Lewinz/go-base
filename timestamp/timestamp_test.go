f012435b60f448911c923679a13d437dc22cb534
lewinz <lewinzheng@mail.com>
2021-08-31 14:29:11 +0800

    update timestemp util test

--- timestamp/timestamp_test.go
+++ timestamp/timestamp_test.go
@@ -1,50 +1,49 @@
-package timestamp_test
+package timestamp
 
 import (
 	"testing"
 	"time"
 
-	"github.com/Lewinz/golang_utils/timestamp"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTimestamp(t *testing.T) {
 	assert := assert.New(t)
 
-	now := timestamp.Now()
+	now := Now()
 	assert.NotNil(now)
 
-	uts := timestamp.Unix(1490328882, 0)
+	uts := Unix(1490328882, 0)
 	assert.NotNil(uts)
 	assert.Equal(uts.Unix(), int64(1490328882))
 
-	date := timestamp.Date(2018, 11, 11, 0, 0, 0, 0, time.Local)
+	date := Date(2018, 11, 11, 0, 0, 0, 0, time.Local)
 	assert.NotNil(date)
 	assert.Equal(date.Year(), 2018)
 	assert.Equal(date.Month(), time.Month(11))
 	assert.Equal(date.Day(), 11)
 
-	timestamp := timestamp.New()
-	assert.NotNil(timestamp)
+	times := New()
+	assert.NotNil(times)
 
-	err := timestamp.UnmarshalJSON([]byte("2018-11-15 17:56:17"))
+	err := times.UnmarshalJSON([]byte("2018-11-15 17:56:17"))
 	assert.Nil(err)
-	assert.False(timestamp.IsZero())
+	assert.False(times.IsZero())
 
-	err = timestamp.UnmarshalJSON([]byte("1490328882"))
+	err = times.UnmarshalJSON([]byte("1490328882"))
 	assert.Nil(err)
-	assert.False(timestamp.IsZero())
+	assert.False(times.IsZero())
 
-	b, err := timestamp.MarshalJSON()
+	b, err := times.MarshalJSON()
 	assert.Nil(err)
 	assert.True(len(b) > 0)
 	assert.Equal(string(b), "1490328882")
 
-	err = timestamp.Scan(time.Now())
+	err = times.Scan(time.Now())
 	assert.Nil(err)
-	assert.False(timestamp.IsZero())
+	assert.False(times.IsZero())
 
-	value, err := timestamp.Value()
+	value, err := times.Value()
 	assert.Nil(err)
 	assert.NotNil(value)
 
